6b6e480fdf27f6291849c6060a254217250bd6d7
Yonatan Koren <yonkornilov@live.com>
2017-08-29 13:08:09 -0400

    using public ip as id

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const os = require('os');
 var io = require('socket.io-client');
 var https = require('https');
 
+var publicIp = require('public-ip');
+
 https.globalAgent.options.rejectUnauthorized = false;
-socket = io.connect(process.env.HTTPS_SERVER, { agent: https.globalAgent });
-socket.on('connect', function(){
-    console.log("connected to server");
-});
-socket.on('request-load-avg', function(data){
-    socket.emit('load-average', {
-        id: socket.id,
-        load: os.loadavg()[0]
+
+publicIp.v4().then(ip => {
+    publicIp = ip;
+    socket = io.connect(process.env.HTTPS_SERVER, { agent: https.globalAgent });
+    socket.on('connect', function(){
+        console.log("connected to server");
+    });
+    socket.on('request-load-avg', function(data){
+        socket.emit('load-average', {
+            id: ip,
+            load: os.loadavg()[0]
+        });
     });
 });
